6b263f3601f202a68379da78ffc35d0f83d78ff0
sutcuremzi <kaanersoy@windowslive.com>
2024-07-06 20:25:33 +0300

    fix lag_columns function

--- smart/utils/column_lagger.py
+++ smart/utils/column_lagger.py
@@ -5,12 +5,12 @@ def lag_columns(df, lag_count, cols):
     lagged_cols = []
 
     for col in cols:
-        lagged_cols = [df[col].shift(
+        lagged_col_list = [df[col].shift(
             lag) for lag in range(1, lag_count + 1)]
-        lagged_df = pd.concat(lagged_cols, axis=1)
-        lagged_df.cols = [
+        lagged_df = pd.concat(lagged_col_list, axis=1)
+        lagged_df.columns = [
             f"{col}_lag_{lag}" for lag in range(1, lag_count + 1)]
-        lagged_cols.extend(lagged_df.cols)
+        lagged_cols.extend(lagged_df.columns)
         df = pd.concat([df, lagged_df], axis=1)
 
     df.dropna(inplace=True)
